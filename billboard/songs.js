989114806e12dff13e0e650a5c997a756f521fd2
travis flake <tflake83@gmail.com>
2022-01-06 06:38:30 -0500

    finally imported correctlty!

--- billboard/songs.js
+++ billboard/songs.js
@@ -1,1 +1,7 @@
-// Import the songs data from the CSV file and export it for use in question files.
+// Import the songs data from the CSV file and export it for use in question files.const fs = require('fs')
+const fs = require('fs')
+const billboard = fs.readFileSync('billboard100_2000.csv', 'utf-8')
+
+module.exports = {
+  top100: billboard.split('\n')
+}

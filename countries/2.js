123d90c3ee550d97cede4c538fad98223cdcaef2
travis flake <tflake83@gmail.com>
2022-01-07 11:32:41 -0500

    done

--- countries/2.js
+++ countries/2.js
@@ -1,11 +1,27 @@
-const countries = require('./countries')
-
+const { countries } = require('./countries')
+// this code works with the || symbol but not with the && symbol???? CANT FIGURE IT OUT
 // What countries both begin and end with a vowel?
+const vowels = ["a","e","i","o","u","A","E","I","O","U"];
 
-function solution(list) {
+function solution(countries) {
 
-  // ...
+  const result = [];
 
+  
+  countries.forEach(country => {
+    vowels.forEach(vowel => {
+
+      if (country.startsWith(vowel) || country.endsWith(vowel)){
+        result.push(country)
+      }
+    })
+  
+    })
+    return result
+  
+
+  
 }
 
-console.log(solution(list))
+console.log(solution(countries));
+

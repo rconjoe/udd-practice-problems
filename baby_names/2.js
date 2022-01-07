e92b088bf7f738a36db8f680f3bdd37cddae8b63
travis flake <tflake83@gmail.com>
2022-01-06 20:17:37 -0500

    new solution

--- baby_names/2.js
+++ baby_names/2.js
@@ -1,10 +1,21 @@
+// const { resourceLimits } = require('worker_threads');
 const { oldNames, newNames } = require('./names')
 
 // What are the longest baby names in the top 40 baby names for 2020?
 // Make sure you can handle if there's a tie.
 
 function solution() {
+  result = [];
+  let longest = newNames[0]; 
 
-  // ...
+    newNames.forEach(newNew => {
+     let notLongest = newNew;
+     if (longest.length < notLongest.length ){
+        longest = notLongest;
+    }  
+  })
+  result.push(longest);
 
+  return result
 }
+console.log(solution());

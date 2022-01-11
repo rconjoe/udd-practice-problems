3f060427e6dfced7d8aa00bf0b76c8cb3c46a42c
rconjoe <joe@rconjoe.com>
2022-01-10 18:57:25 -0800

    wip billboard #1

--- billboard/1.js
+++ billboard/1.js
@@ -9,3 +9,97 @@
  * "Smooth" - Santana Featuring Rob Thomas
  * "Independent Women Part I" - Destiny's Child
  */
+
+const { top100 } = require('./songs')
+
+function solution() {
+
+  /**
+   * First we initialize an empty array that will hold our songs, split into 52 arrays, one song per week.
+   * We also remove the first line from the list, since that line just has the column names.
+   */
+  const year = []
+  top100.shift()
+
+  /**
+   * For each week in the year (52), 
+   * we splice the first 100 songs off of the original array of all songs.
+   * 
+   * Then, for each song in that group of 100,
+   * we split the fields up at each comma.
+   * 
+   * We assign those fields to a song object,
+   * then push the song to our week array.
+   * 
+   * After each iteration through 100 songs, 
+   * we push the resulting week array to our year array.
+   *
+   * 
+   * Reference of how the year should look: 
+   * [
+   *   [
+   *     {
+   *       rank: 1,
+   *       song: 'song name',
+   *       artist: 'artist name,
+   *       last_week: 12,
+   *       peak_rank: 1,
+   *       ... etc
+   *     },
+   *     {
+   *       rank: 1,
+   *       song: 'song name',
+   *       artist: 'artist name,
+   *       last_week: 12,
+   *       peak_rank: 1,
+   *       ... etc
+   *     },
+   *   ],
+   *   [
+   *     {
+   *       rank: 1,
+   *       song: 'song name',
+   *       artist: 'artist name,
+   *       last_week: 12,
+   *       peak_rank: 1,
+   *       ... etc
+   *     },
+   *     {
+   *       rank: 1,
+   *       song: 'song name',
+   *       artist: 'artist name,
+   *       last_week: 12,
+   *       peak_rank: 1,
+   *       ... etc
+   *     }, 
+   *     ...100 songs
+   *   ],
+   *   ...52 weeks
+   * ] 
+   * 
+   * 
+   */
+
+  for (i=0; i<53; i++) {
+    const week = []
+    const rawSongs = top100.splice(0, 100)
+    rawSongs.forEach(rawSong => {
+      const fields = rawSong.split(',')
+      const song = {
+        rank: fields[0],
+        song: fields[1],
+        artist: fields[2],
+        last_week: fields[3],
+        peak_rank: fields[4],
+        weeks_on_board: fields[5],
+        date: fields[6]
+      }
+      week.push(song)
+    })
+    year.push(week)
+  }
+
+}
+
+
+console.log(solution())
--- billboard/songs.js
+++ billboard/songs.js
@@ -1,1 +1,8 @@
 // Import the songs data from the CSV file and export it for use in question files.
+
+const fs = require('fs')
+const songs = fs.readFileSync('billboard/billboard100_2000.csv', 'utf-8')
+
+module.exports = {
+  top100: songs.split('\n')
+}

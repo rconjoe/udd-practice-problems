1857fc94a4913d121017b87fa206021412b50361
travis flake <tflake83@gmail.com>
2022-01-11 16:59:03 -0500

    just finished my 1s one.. sure it needs improvement

--- billboard/1.js
+++ billboard/1.js
@@ -1,3 +1,4 @@
+const { removeAllListeners } = require('process');
 const songs = require('./songs')
 const { top100 } = require('./songs')
 
@@ -14,33 +15,76 @@ const { top100 } = require('./songs')
  */
 
 
-let rawSongs = [];
-let y = {}
-    // weeks of year
-    for (var i = 0; i < 53; i++) {
-        // songs
-        for (var i = 0; i < 100; i++) {
-            top100.forEach(top => {
-                [{
-                    rank: [0],
-                    song: [1],
-                    artist: [2],
-                    last_week: [3],
-                    peak_week: [4],
-                    weeks_on_board: [5],
-                    date: [6]
-                }]
-                return rawSongs.push(top.split('\,').map(top))
-            })
+/**
+ * Print out all of the #1 songs and the artists who made them. 
+ * If a song was #1 for more than one week, only print it once.
+ * Example output: 
+ * "Try Again" - Aaliyah
+ * "Say My Name" - Destiny's Child
+ * "What A Girl Wants" - Christina Aguilera
+ * "Maria Maria" - Santana Featuring The Product G&B
+ * "Smooth" - Santana Featuring Rob Thomas
+ * "Independent Women Part I" - Destiny's Child
+ */
 
 
+ function solution() {
+ 
+   /**
+    * First we initialize an empty array that will hold our songs, split into 52 arrays, one song per week.
+    * We also remove the first line from the list, since that line just has the column names.
+    */
+   const year = []
+   top100.shift()
+   let  number1 =[];
+ 
+   /**
+    * For each week in the year (52), 
+    * we splice the first 100 songs off of the original array of all songs.
+    * 
+    * Then, for each song in that group of 100,
+    * we split the fields up at each comma.
+    * 
+    * We assign those fields to a song object,
+    * then push the song to our week array.
+    * 
+    * After each iteration through 100 songs, 
+    * we push the resulting week array to our year array.
+    *
+    * 
+    */
+ 
+   for (i=0; i<53; i++) {
+     const week = []
+     const rawSongs = top100.splice(0, 100)
+     rawSongs.forEach(rawSong => {
+       const fields = rawSong.split(',')
+       const song = {
+         rank: fields[0],
+         song: fields[1],
+         artist: fields[2],
+         last_week: fields[3],
+         peak_rank: fields[4],
+         weeks_on_board: fields[5],
+         date: fields[6]
+       }
+       if (fields[0]===('1')){
+         number1.push(fields[1]+ ' - '  +fields[2]) 
+         
 
-        }     
+       }
 
-    }
+     })
+   }
 
-    console.log(rawSongs)
+   let removeRepeats =  new Set (number1)
 
+   console.log(removeRepeats)
+
+
+ }
+
+ solution()
 
 
 

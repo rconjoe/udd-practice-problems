850964d3a0bf108faf7f203b95ad9a18bd384fb8
travis flake <tflake83@gmail.com>
2022-01-29 18:38:17 -0500

    data

--- nba_finals/nbaData.js
+++ nba_finals/nbaData.js
@@ -1,2 +1,9 @@
 // Read the NBA finals CSV data into one or more dictionaries as needed to complete the questions.
 // Export it as needed to use in the question files.
+
+const fs = require ('fs');
+const nba = fs.readFileSync("nba_finals.csv", 'utf-8');
+
+module.exports = {
+    nbaFinals: nba.split('\n')
+}

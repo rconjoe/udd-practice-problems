b201b993732d176c45aad1df9acf127c0334ffd2
travis flake <tflake83@gmail.com>
2022-01-31 09:14:46 -0500

    another one !

--- movies/2.js
+++ movies/2.js
@@ -9,9 +9,19 @@ function solution(){
     allMovies.forEach(movie => {
         let values = movie.split(',')
         let keys = {
-            
+            title: values[0],
+            distributer: values[1],
+            sales: values[3]
         }
+        listOfMovies.push(keys)
     })
 
+    let highGross = listOfMovies.filter(movie => movie.distributer === 'Universal Pictures').sort(function(a, b){return a -b})
+
+
+
+    let highDomestic = []
+    console.log(highGross)
 }
 solution()
+// Title,Distributor,Release Date,US Sales,World Sales,Runtime,Rating

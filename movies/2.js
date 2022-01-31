04b604590ef306635b238355d72401f1c2badc2f
travis flake <tflake83@gmail.com>
2022-01-31 08:40:14 -0500

    2.js

---
+++ movies/2.js
@@ -0,0 +1,17 @@
+// What is the highest grossing moving from Universal Pictures, domestically?
+
+const {movies} = require('./filmsDate.js')
+
+function solution(){
+    const allMovies = movies;
+
+    let listOfMovies = []
+    allMovies.forEach(movie => {
+        let values = movie.split(',')
+        let keys = {
+            
+        }
+    })
+
+}
+solution()

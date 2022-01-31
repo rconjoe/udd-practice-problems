c4c9a040a7e6decb99888f9352bb5173380c3eb5
travis flake <tflake83@gmail.com>
2022-01-31 08:34:09 -0500

    im feeling great! ..lol

--- movies/1.js
+++ movies/1.js
@@ -2,7 +2,28 @@
 
 const {movies} = require('./filmsDate.js')
 
-function solution({
+function solution(){
     const allMovies = movies
-})
+
+    //this block is seperating the data
+    let listOfMovies =[]
+    allMovies.forEach(movie =>{
+        let values = movie.split(',')
+        let keys = {
+            title: values[0],
+            distributor: values[1],
+        }
+        listOfMovies.push(keys)
+    })
+
+    //filter movies by dreamworks
+    let dreamWorks = listOfMovies.filter(movie => movie.distributor === 'DreamWorks')
+
+
+
+    console.log(dreamWorks)
+  
+    
+
+}
 solution()
